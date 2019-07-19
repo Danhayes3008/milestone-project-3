14be62c99880df075a0c3103fa7a20676304bd3e
Ubuntu <ubuntu@ip-172-31-89-197.ec2.internal>
2019-07-19 20:00:35 +0000

    added the routes for the login page and recipe page

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+    
+@app.route('/recipes')
+def recipes():
+    return render_template('recipes.html')
+    
+@app.route('/login')
+def login():
+    return render_template('Login.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
