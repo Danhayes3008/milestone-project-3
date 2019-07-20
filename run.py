818fac73c6b2d3e398573c5455a8463011fcf702
Ubuntu <ubuntu@ip-172-31-89-197.ec2.internal>
2019-07-20 08:24:28 +0000

    added a secret key to the __name__ if statement and imported bcrypt

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
+from flask_login import LoginManager, UserMixin
+import bcrypt
 
 app = Flask(__name__)
 
@@ -17,6 +19,7 @@ def login():
     return render_template('Login.html')
 
 if __name__ == '__main__':
+    app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
