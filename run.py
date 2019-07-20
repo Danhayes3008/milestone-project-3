0648fe3a0eadeb1ddbf2cff59cb86c754e944c07
Ubuntu <ubuntu@ip-172-31-89-197.ec2.internal>
2019-07-20 08:39:50 +0000

    added the pymongo extention, removed some imports that was not needed and linked the run.py to my mongo database

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
-from flask_login import LoginManager, UserMixin
+from flask import Flask, render_template, redirect, request, url_for, session
+from flask.ext.pymongo import PyMongo
 import bcrypt
 
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'Bakery_Wikipedia'
+
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/home')
