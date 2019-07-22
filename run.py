d5b7b88762bcbc4fa1aa65de78987436340e1559
Ubuntu <ubuntu@ip-172-31-89-197.ec2.internal>
2019-07-22 13:44:35 +0000

    finished the loggin system allowing people to create and account and log into there account

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask, render_template, url_for, request, session, redirect
+from flask import Flask, render_template, url_for, request, session, redirect, flash
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 import bcrypt
 
 
 app = Flask(__name__)
-username = session['name']
 
 
 app.config["MONGO_DBNAME"] = 'Bakery_Wikipedia'
@@ -16,47 +16,53 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    if 'name' in session:
-        return 'You are already logged in as ' + session['name']
     
     return render_template('index.html')
     
     
 @app.route('/recipes')
 def recipes():
-    if 'name' in session:
-        return 'You are already logged in as ' + session['name']
     
     return render_template('recipes.html')
     
+
     
 @app.route('/login')
 def login():
     return render_template('Login.html')
     
-
-@app.route('/find_user', methods=['POST'])
-def find_user():
-    if request.method == 'POST':
-        users = mongo.db.users
-        session = users.find({'name': request.form['name']})
-        
-        if session is None:
-            return 'No user exists with this name!'
-            
-        if request.form['pass'] == 'password' and request.form['name'] == 'users':
-            session['logged_in'] = True
-            return session['name']
+@app.route('/user_login', methods=['GET', 'POST'])
+def user_login():
+    """Function for handling the logging in of users"""
+    user = mongo.db.users
+    logged_in_user = user.find_one({
+            'name': request.form['name']})
+    print(logged_in_user)
+                                
+    if logged_in_user: # If the username is in the db then check the password
+        if logged_in_user['password'] == request.form['pass']: # If password matches
+            session['name'] = request.form['name'] #Set the session username
+            session['logged_in'] = True #Set the logged_in flag
+            print('Going to the users route')
+            return redirect(url_for('users'))
+        print('Sorry wrong pass')
+        flash('Sorry incorrect password!') #If password is incorrect
+        return redirect(url_for('login'))
+    flash('sorry something went wrong')
+    return redirect(url_for('login'))
     
-    return render_template('userpage.html', users=mongo.db.users.find())
+    
+@app.route('/logout')
+def logout():
+    """Logs the user out and redirects to home"""
+    session.clear()  # Kill session
+    return redirect(url_for('index'))
     
     
     
 @app.route('/users')
 def users():
-    if 'name' in session:
-        return 'You are already logged in as ' + session['name']
-        
+    
     return render_template('userpage.html', users=mongo.db.users.find())
     
     
@@ -65,23 +71,27 @@ def register():
     return render_template('register.html')
 
 
-@app.route('/insert_user', methods=['POST'])
+@app.route('/insert_user', methods=['GET', 'POST'])
 def insert_user():
     if request.method == 'POST':
-        users = mongo.db.users
-        existing_user = users.find({'name': request.form['name']})
+        user = mongo.db.users  #Load your user collection
+        existing_user = user.find_one({'name': request.form['name']}) #Check if user already exists
         
-       
         
-        if existing_user is None:
-            tasks = mongo.db.users
-            tasks.insert_one(request.form.to_dict())
-            return redirect(url_for('index'))
-            
-        return 'That username already exists!'
-    return render_template('register.html')
+        if existing_user is None:  #If user doesn't exist then lets register them
+            user.insert(request.form.to_dict()) #I'd map the fields manually and not use to_dict() method..
+            session['name'] = request.form['name'] #Upon successful registration set the username to the session
+            print(session['name'])
+            session['logged_in'] = True # Set a indicator to help user later on
+            flash('You are now registered')
+            return redirect(url_for('users'))
+        flash('Sorry username already exists!')
+        return redirect(url_for('register'))
+	
+    return render_template('userpage.html')
 
 if __name__ == '__main__':
+    app.secret_key = 'Hello'
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
